9ce76e4a151ab3fc361dc7020861ad465989eb8f
mmddrr1 <110559882+mmddrr1@users.noreply.github.com>
2024-01-17 21:14:06 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["👑 Crée  par g_riz_zly"];
 
 
 let currentIndex = 0;
@@ -93,7 +93,7 @@ function updateStatusAndSendMessages() {
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️Wtg`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
